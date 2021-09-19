6af86020929304aacc90ac26e35b3ad0fdee629c
MatheusSoares <matheuzoares@gmail.com>
2021-09-19 14:35:37 -0300

    aula02

--- aula01/exercicio04.java
+++ aula01/exercicio04.java
@@ -1,8 +1,10 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class exercicio04 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
 
         double quilowatt, salario, qtdQuilowatts, pagamento, desconto;
 
@@ -16,9 +18,9 @@ public class exercicio04 {
         pagamento = qtdQuilowatts * quilowatt;
         desconto = pagamento * 0.85;
 
-        System.out.printf("O valor de cada quilowatt é %.2f", quilowatt);
-        System.out.printf("O valor a ser pago é %.2f", pagamento);
-        System.out.printf("O valor com desconto é %.2f", desconto);
+        System.out.printf(Locale.US, "O valor de cada quilowatt é %.2f", quilowatt);
+        System.out.printf(Locale.US, "O valor a ser pago é %.2f", pagamento);
+        System.out.printf(Locale.US, "O valor com desconto é %.2f", desconto);
 
         entrada.close();
     }
--- aula02/exercicio01.java
+++ aula02/exercicio01.java
@@ -3,5 +3,20 @@ import java.util.Scanner;
 public class exercicio01 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+
+        int numero, metade;
+
+        System.out.println("Digite um número.");
+        numero = entrada.nextInt();
+
+        metade = numero/2;
+
+        if (numero > 20) {
+            System.out.printf("A metade do número é %d", metade);
+        } else {
+            System.out.printf("O número é %d", numero);
+        }
+
+        entrada.close();
     }
 }
