06a4a044c19bcd7445d748ecbc74a0bf0396f8a5
MatheusSoares <matheuzoares@gmail.com>
2021-09-19 14:12:49 -0300

    mais

--- aula01/exercicio03.java
+++ aula01/exercicio03.java
@@ -1,7 +1,20 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class exercicio03 {
     public static void main(String[] args) {
-        
+        Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
+
+        double salario, aumento;
+
+        System.out.println("Digite o salário:");
+        salario = entrada.nextDouble();
+
+        aumento = salario * 1.25;
+
+        System.out.printf(Locale.US, "O aumento é de %.2f", aumento);
+
+        entrada.close();
     }
 }
